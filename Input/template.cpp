6753870f2082a530069578732a3142490d6f89d6
Colossus <33657750+CQLLL@users.noreply.github.com>
2020-04-24 22:02:51 +0800

    Update template.cpp

--- Input/template.cpp
+++ Input/template.cpp
@@ -1,1 +1,31 @@
-#include
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<vector<int>> test;
+    vector<int> v;
+    int n,temp;
+ 
+    cin >> n;
+    test.clear();
+    for (int i = 0; i<n; i++) {
+        v.clear();
+        for (int j = 0; j < n; j++)
+        {
+            cin >> temp;
+            v.push_back(temp);
+        }
+        test.push_back(v);
+    }
+  
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0;j < n; j++)
+        {
+            cout << test[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
